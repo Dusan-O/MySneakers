55b8f5dd3e13f68c57e2392b751ecddae727661d
Dusan-O <dusan.orescanin@gmail.com>
2022-08-12 00:32:22 +0200

    resolution protocol error - app running: ok

--- MySneakers/ViewController.swift
+++ MySneakers/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController {
     
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var typeSegmented: UISegmentedControl!
