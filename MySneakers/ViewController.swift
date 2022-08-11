e9c00b9875e17384f06e0c7cc7c688ac7dda07cb
Dusan-O <dusan.orescanin@gmail.com>
2022-08-11 18:57:57 +0200

    Config typeSegmented, Stepper and get interactions

--- MySneakers/ViewController.swift
+++ MySneakers/ViewController.swift
@@ -16,24 +16,64 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var colorPicker: UIPickerView!
     @IBOutlet weak var resultImageView: UIImageView!
     @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var sizeLabel: UILabel!
+    @IBOutlet weak var genderLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTextField()
+        updateGenderLabel()
+        updateSizeLabel()
     }
     
+    func getImage() {
+        let imageString = getGender() + "_" + getType() + "_" + getColor()
+        print(imageString)
+    }
+
+    func getType() -> String {
+        switch typeSegmented.selectedSegmentIndex {
+        case 0: return "city"
+        case 1: return "running"
+        default: return "basket"
+        }
+    }
+    
+    func getGender() -> String {
+        return genderSwitch.isOn ? "boy" : "girl"
+    }
+    
+    func getColor() -> String {
+        return ""
+    }
+    
+    func updateGenderLabel() {
+        let str = genderSwitch.isOn ? "Homme" : "Femme"
+        genderLabel.text = str
+    }
+    
+    func updateSizeLabel() {
+        let sizeString = "Pointure: \(Int(sizeStepper.value))"
+        sizeLabel.text = sizeString
+    }
     
     @IBAction func segmentedChanged(_ sender: UISegmentedControl) {
+        getImage()
     }
     
     
     @IBAction func switchChanged(_ sender: UISwitch) {
+        updateGenderLabel()
+        getImage()
     }
     
     
     @IBAction func stepperChanged(_ sender: UIStepper) {
+        updateSizeLabel()
+        getImage()
     }
-    
+
 
     }
 
