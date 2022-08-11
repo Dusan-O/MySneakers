4cded40e334cf51f609a67deac5d03294c7ef133
Dusan-O <dusan.orescanin@gmail.com>
2022-08-11 18:11:40 +0200

    Main view + IBOutlets & IBActions connected

--- MySneakers/ViewController.swift
+++ MySneakers/ViewController.swift
@@ -8,11 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var typeSegmented: UISegmentedControl!
+    @IBOutlet weak var genderSwitch: UISwitch!
+    @IBOutlet weak var sizeStepper: UIStepper!
+    @IBOutlet weak var colorPicker: UIPickerView!
+    @IBOutlet weak var resultImageView: UIImageView!
+    @IBOutlet weak var resultLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func segmentedChanged(_ sender: UISegmentedControl) {
+    }
+    
+    
+    @IBAction func switchChanged(_ sender: UISwitch) {
+    }
+    
+    
+    @IBAction func stepperChanged(_ sender: UIStepper) {
+    }
+    
+    
+    
+    
+    
 
 
 }
